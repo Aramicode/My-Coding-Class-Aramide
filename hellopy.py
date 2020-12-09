66bddc45834b91532e550b6dca36e5ba2b3e98e0
Aramide Ojudun <69688689+Aramicode@users.noreply.github.com>
2020-12-09 23:24:16 +0200

    Add files via upload
    First Python class activity with Aladi

---
+++ hellopy.py
@@ -0,0 +1,23 @@
+print ("Hello World")
+print("Hey World, This is my first python class!")
+message = "This is a new class"
+print(message)
+
+# Write a program that calculates the area of a square
+# Write the formula: Length * Length
+# Measure the length using a davice
+# Ask the User for the length
+# Calculate the area of the square using the formula(Length * Length)
+# We are going to hardcode(opp is dynamic) values; No dynamic values
+length = 4
+# Calculate the area of the square using the formula(Length * Length)
+area = length * length
+print(area)
+# Convert your program to accept user input for the length variable and use the
+# input to compute the area of the square.
+
+#First Python Assignment. Create a program that displays your name and complete
+#mailing add formatted in an envelope address manner. Your program does not
+#need to read any input from the user
+#2) Using only the concept taught in class, write a program that accepts a
+#user's name and prints it 10times
